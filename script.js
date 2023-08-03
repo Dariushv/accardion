c940f74e5fbd8499abd76d32150b6ddcbe4bf056
Dariushv <vahabdariush@gmail.com>
2023-08-03 15:03:20 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const accordionItems = document.querySelectorAll('.accordion-item');
+  
+    accordionItems.forEach(item => {
+      const header = item.querySelector('.accordion-header');
+      const content = item.querySelector('.accordion-content');
+  
+      header.addEventListener('click', () => {
+        content.classList.toggle('active');
+        if (content.classList.contains('active')) {
+          content.style.display = 'block';
+        } else {
+          content.style.display = 'none';
+        }
+      });
+    });
+  });
+  
